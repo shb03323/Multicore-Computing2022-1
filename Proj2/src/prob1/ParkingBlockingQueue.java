88c993a6d3860c79dd42a77af07a2d7d52897528
jeonghoon <shb03323@naver.com>
2022-05-10 20:46:13 +0900

    [ADD] problem1

---
+++ Proj2/src/prob1/ParkingBlockingQueue.java
@@ -0,0 +1,84 @@
+package prob1;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ParkingBlockingQueue {
+    public static void main(String[] args) {
+        ParkingGarage parkingGarage = new ParkingGarage(7);
+        for (int i=1; i<= 10; i++) {
+            Car c = new Car("Car "+i, parkingGarage);
+            c.start();
+        }
+    }
+}
+
+class ParkingGarage {
+    private ArrayBlockingQueue<String> places;
+    public ParkingGarage(int capacity) {
+        if (capacity < 0)
+            capacity = 0;
+        places = new ArrayBlockingQueue<>(capacity);
+    }
+    public void enter(String name) { // enter parking garage
+        try {
+            places.put(name);
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+    public void leave() { // leave parking garage
+        try {
+            places.take();
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Car extends Thread {
+    private ParkingGarage parkingGarage;
+    public Car(String name, ParkingGarage p) {
+        super(name);
+        this.parkingGarage = p;
+    }
+
+    private void tryingEnter()
+    {
+        System.out.println(getName()+": trying to enter");
+    }
+
+
+    private void justEntered()
+    {
+        System.out.println(getName()+": just entered");
+
+    }
+
+    private void aboutToLeave()
+    {
+        System.out.println(getName()+":                                     about to leave");
+    }
+
+    private void Left()
+    {
+        System.out.println(getName()+":                                     have been left");
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                sleep((int)(Math.random() * 10000)); // drive before parking
+            } catch (InterruptedException e) {}
+            tryingEnter();
+            parkingGarage.enter(getName());
+            justEntered();
+            try {
+                sleep((int)(Math.random() * 20000)); // stay within the parking garage
+            } catch (InterruptedException e) {}
+            aboutToLeave();
+            parkingGarage.leave();
+            Left();
+        }
+    }
+}
